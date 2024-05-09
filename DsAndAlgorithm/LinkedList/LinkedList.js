d079d7e14f79bf6d8645b438d3e4019c1026ec45
Heisnam Sunrose <johnsonheisnam02@gmail.com>
2024-05-09 12:33:12 +0530

    linkedlist initial commit

---
+++ DsAndAlgorithm/LinkedList/LinkedList.js
@@ -0,0 +1,19 @@
+/* what is Linked List?
+
+Array is of fixed length.
+Array is stored in the heap memory in contiguous location.
+Array is easy to traverse with index.
+We cannot increase or decrease the size of the array.
+
+Linked List
+-------------------------------------------------------------
+This where LinkedList comes.
+Linked List can increase or decrease its size.
+Linked List are stored in the heap memory but not in contiguous memory location.
+Hence we can easily increase or decrease the size of the Linked List.
+We cannot traverse with index.
+
+Each node contains data and pointer to the next node.
+Head Node and Tail Node
+-------------------------------------------------------------
+*/
