6561a628bc5872d6f5f34b137eabe2ff53a12518
Heisnam Sunrose <johnsonheisnam02@gmail.com>
2024-06-23 23:41:08 +0530

    sortLL

--- DsAndAlgorithm/LinkedList/1.11.SortLL.js
+++ DsAndAlgorithm/LinkedList/1.11.SortLL.js
@@ -11,6 +11,10 @@
   SC = > O(N)
 */
 
+const sortArray = (arr) => {
+  // any sorting algorithm
+};
+
 const sortLL = (head) => {
   let arr = [];
   let currentNode = head;
@@ -19,7 +23,7 @@ const sortLL = (head) => {
     arr.push(currentNode.data);
     currentNode = currentNode.next;
   }
-  arr = sort(arr);
+  arr = sortArray(arr);
 
   currentNode = head;
   let index = 0;
@@ -35,5 +39,111 @@ const sortLL = (head) => {
 /*
   Merge Sort is simpler to implement
   Quick Sort is tougher to implement 
+
+  TC  => O(log N) * O(N + N/2) ~ O(N logN) 
+    SC => O(1) 
+
+  Note => multiple function then preferable use a class 
 */
-const sortLlMS = (head) => {};
+
+// TC =>  O(N/2)
+const middle = (head) => {
+  let tortoise = head;
+  // LL with even nodes gets splitted at the first half
+  let hare = head.next;
+  while (hare && hare.next) {
+    tortoise = tortoise.next;
+    hare = hare.next.next;
+  }
+
+  return tortoise;
+};
+
+// TC =>  O(N)
+const merge = (head1, head2) => {
+  let dummyNode = new Node(-1);
+  let mover = dummyNode;
+
+  while (head1 && head2) {
+    if (head1.data <= head2.data) {
+      mover.next = head1;
+      head1 = head1.next;
+    } else {
+      mover.next = head2;
+      head2 = head2.next;
+    }
+    mover = mover.next;
+  }
+
+  mover.next = head1 || head2;
+
+  return dummyNode.next;
+};
+
+// TC =>  O(log N)
+const divide = (head) => {
+  // Base condition
+  if (head === null || head.next === null) return head;
+
+  let leftHead = middle(head);
+  let rightHead = leftHead.next;
+  leftHead.next = null;
+
+  leftHead = divide(leftHead);
+  rightHead = divide(rightHead);
+  return merge(leftHead, rightHead);
+};
+
+const sortLLMS = (head) => {
+  return divide(head);
+};
+
+/*
+  Sample Solution
+*/
+var sortList = function (head) {
+  if (!head || !head.next) {
+    return head;
+  }
+
+  const merge = (left, right) => {
+    const dummy = new ListNode(-1);
+    let curr = dummy;
+
+    while (left && right) {
+      if (left.val <= right.val) {
+        curr.next = left;
+        left = left.next;
+      } else {
+        curr.next = right;
+        right = right.next;
+      }
+      curr = curr.next;
+    }
+
+    curr.next = left || right;
+
+    return dummy.next;
+  };
+
+  const findMiddle = (node) => {
+    let slow = node;
+    let fast = node.next;
+
+    while (fast && fast.next) {
+      slow = slow.next;
+      fast = fast.next.next;
+    }
+
+    return slow;
+  };
+
+  const mid = findMiddle(head);
+  const rightHead = mid.next;
+  mid.next = null;
+
+  const sortedLeft = sortList(head);
+  const sortedRight = sortList(rightHead);
+
+  return merge(sortedLeft, sortedRight);
+};
